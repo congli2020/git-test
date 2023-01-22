aa15278fe7361e2e8bdd5eea0272dca07162fbfb
Cong <licong2020@qq.com>
2023-01-22 23:27:10 +0800

    third commit

--- src/main/java/com/cong/git/GitTest.java
+++ src/main/java/com/cong/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("江西应用技术职业学院");
+        System.out.println("信息工程学院");
     }
 }
