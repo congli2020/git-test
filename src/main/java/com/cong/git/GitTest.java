8aea30d46b5783c2ee0e00c6d37df1101abb1b8f
Cong <licong2020@qq.com>
2023-01-22 23:41:04 +0800

    hot-fix commit2

--- src/main/java/com/cong/git/GitTest.java
+++ src/main/java/com/cong/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("江西应用技术职业学院");
         System.out.println("信息工程学院");
         System.out.println("22物联网0102班");
+        System.out.println("hot-fix test");
     }
 }
