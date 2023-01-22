3671149e82c12d9d62a8904d9d6c36e68cc36bf6
Cong <licong2020@qq.com>
2023-01-22 23:25:40 +0800

    second commit

--- src/main/java/com/cong/git/GitTest.java
+++ src/main/java/com/cong/git/GitTest.java
@@ -3,5 +3,6 @@ package com.cong.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("江西应用技术职业学院");
     }
 }
